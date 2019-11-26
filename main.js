4ff1ea06205309cecb19cf50aa76a26e1a3d1abe
zhangjiale <zhangjiale@btzh.cn>
2019-11-26 17:48:22 +0800

    删除多余组件，login提取h5专用页面

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App'
 
-import pageHead from './components/page-head.vue'
-import pageFoot from './components/page-foot.vue'
 import uLink from '@/components/uLink.vue'
 import store from './store'
 const config = require('@/common/config.js')
@@ -15,10 +13,6 @@ Vue.config.productionTip = false
 Vue.store = Vue.prototype.$store = store
 Vue.cg = Vue.prototype.$cg = config
 Vue.use(gd, {api: config.BASE_URL, key: config.STORAGE_KEY})
-
-Vue.component('page-head', pageHead)
-Vue.component('page-foot', pageFoot)
-Vue.component('uLink', uLink)
 
 App.mpType = 'app'
 
